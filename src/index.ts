09c6f6b61748947d8f653770b3e636fd29dbdc7a
manuelbamise <etobi1930@gmail.com>
2024-12-11 10:22:55 +0100

    working through the problem

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,17 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+const string = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+`;
+
+const character = string.split("");
+
+console.log(character[0]);
+
+
+
+
 app.listen(PORT, () => {
   console.log("app is listening on port 3000");
 });
