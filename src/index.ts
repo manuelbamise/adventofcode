35df548bbeceb589d94f1459549ee90d08be8257
manuelbamise <etobi1930@gmail.com>
2024-12-12 08:22:01 +0100

    working on the problem#

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,21 @@ const PORT = process.env.PORT || 3000;
 
 const string = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 `;
-
+const solutionStore:any[] = [];
 const character = string.split("");
 
-console.log(character[0]);
+//console.log(character[0]);
 
+for (let index = 0; index < character.length; index++) {
+  //console.log(character[index]);
+  
+  if (character[index] === "m"){
+    console.log(character[index]);
+    solutionStore.push(character[index]);
+  }
+}
+
+console.log(solutionStore);
 
 
 
