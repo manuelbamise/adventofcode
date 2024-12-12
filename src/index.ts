ae4c5a7aa8db673d37f391803dc6a2e8903600a7
manuelbamise <etobi1930@gmail.com>
2024-12-12 08:32:55 +0100

    working on the problem

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,24 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
 const string = `xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 `;
-const solutionStore:any[] = [];
+const solutionStore: any[] = [];
 const character = string.split("");
 
 //console.log(character[0]);
 
 for (let index = 0; index < character.length; index++) {
   //console.log(character[index]);
-  
-  if (character[index] === "m"){
-    console.log(character[index]);
-    solutionStore.push(character[index]);
+
+  if (character[index] === "m") {
+    console.log(index);
+    
+    solutionStore.push(character[index+ 1])
   }
 }
 
 console.log(solutionStore);
-
-
 
 app.listen(PORT, () => {
   console.log("app is listening on port 3000");
