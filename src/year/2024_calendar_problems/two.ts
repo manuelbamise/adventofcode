e3e3a20e2b6812dd42cac8673a14444297bb881d
manuelbamise <etobi1930@gmail.com>
2025-09-04 11:12:35 +0100

    process: routine save

--- src/year/2024_calendar_problems/two.ts
+++ src/year/2024_calendar_problems/two.ts
@@ -8,7 +8,7 @@ const two_main = () => {
 8 6 4 4 1
 1 3 6 7 9`
 
-  const result = data
+  const array_Numbers = data
     .trim()
     .split("\n")
     .map(line =>
@@ -18,6 +18,34 @@ const two_main = () => {
         .map(Number)
     );
 
+  const safe = 0;
+  const unsafe = 0;
+  const least_decreasing = 1;
+  const most_increasing = 3;
+
+  for (let i = 0; i < array_Numbers.length; i++) {
+    const another_array = array_Numbers[i]
+    // console.log(another_array);
+
+    if (another_array) {
+      for (let i2 = 0; i2 < another_array.length; i2++) {
+        const level = another_array[i2]
+        const level_infront = another_array[i2 + 1]
+
+        if (level && level_infront) {
+          const difference = level_infront - level;
+          const absoluteValue = Math.abs(difference)
+
+          if (absoluteValue > most_increasing) {
+            console.log(level, level_infront);
+          }
+          console.log("End of a report\n");
+
+        }
+      }
+
+    }
+  }
 
 }
 
