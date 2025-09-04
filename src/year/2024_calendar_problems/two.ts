3707b0f6ee75a67be4879699407148f845fa3e45
manuelbamise <etobi1930@gmail.com>
2025-09-04 12:00:41 +0100

    process: i'm going crazy with this code

--- src/year/2024_calendar_problems/two.ts
+++ src/year/2024_calendar_problems/two.ts
@@ -18,33 +18,43 @@ const two_main = () => {
         .map(Number)
     );
 
-  const safe = 0;
-  const unsafe = 0;
+  let safe = 0;
+  let unsafe = 0;
   const least_decreasing = 1;
   const most_increasing = 3;
 
-  for (let i = 0; i < array_Numbers.length; i++) {
-    const another_array = array_Numbers[i]
-    // console.log(another_array);
+  const check_another_array = (a: number[]): number => {
+    for (let l = 0; l < a.length; l++) {
+      const lev = a[l]
+      const lev_front = a[l + 1]
 
-    if (another_array) {
-      for (let i2 = 0; i2 < another_array.length; i2++) {
-        const level = another_array[i2]
-        const level_infront = another_array[i2 + 1]
+      if (lev_front && lev) {
+        // console.log(lev, lev_front);
+        const absolute_value = Math.abs(lev_front - lev)
+        console.log(lev, lev_front, absolute_value);
 
-        if (level && level_infront) {
-          const difference = level_infront - level;
-          const absoluteValue = Math.abs(difference)
-
-          if (absoluteValue > most_increasing) {
-            console.log(level, level_infront);
-          }
-          console.log("End of a report\n");
-
+        if (absolute_value > most_increasing || absolute_value < least_decreasing) {
+          safe++;
         }
+        unsafe++;
       }
+    }
+    return 1;
+  }
+
+  for (let i = 0; i < array_Numbers.length; i++) {
+    const reports = array_Numbers[i]
+    if (reports) {
+      console.log(reports);
+      safe = 0;
+      unsafe = 0;
+
+      const levels = check_another_array(reports)
+
 
     }
+    console.log(safe, unsafe);
+
   }
 
 }
