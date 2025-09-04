08a1fc77d747ea25668f0a7fe1bae2999361a488
manuelbamise <etobi1930@gmail.com>
2025-09-04 10:33:31 +0100

    process: split the string into arrays of numbers

--- src/year/2024_calendar_problems/two.ts
+++ src/year/2024_calendar_problems/two.ts
@@ -1,5 +1,24 @@
 const two_main = () => {
   console.log("this is from the two file in the 222");
+  const data = `
+    7 6 4 2 1 
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
+
+  const result = data
+    .trim()
+    .split("\n")
+    .map(line =>
+      line
+        .trim()
+        .split(/\s+/)
+        .map(Number)
+    );
+
+
 }
 
 
